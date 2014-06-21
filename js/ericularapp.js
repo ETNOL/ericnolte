d3b43c6269c4c6210c79ea500f8a70165b0f36b3
Eric Nolte <amishe@gmail.com>
2014-06-21 17:35:25 -0400

    add sidemenu

--- js/ericularapp.js
+++ js/ericularapp.js
@@ -2,6 +2,78 @@
 
 	var app = angular.module("ericularApp", []);
 
+	app.directive("navBar", function(){
+		return {
+			restrict:"E",
+			templateUrl:"../layouts/nav-bar.html"
+		};
+	});
+
+	app.directive("customDirective", function() {
+		return {
+			restrict:"E",
+			templateUrl:"../custom-directive.html"
+		};
+	});
+
+	app.directive("footer", function() {
+		return {
+			restrict:"E",
+			templateUrl:"../layouts/footer.html"
+		};
+	});
+
+	app.directive("indexMain", function() {
+		return {
+			restrict:"E",
+			templateUrl:"../indexMain.html"
+		};
+	});
+
+	app.directive("projectsMain", function() {
+		return {
+			restrict:"E",
+			templateUrl:"../projectsMain.html"
+		};
+	});
+
+	app.directive("sideMenu", function() {
+			return {
+				restrict:"E",
+				templateUrl:"../layouts/sidemenu.html"
+			}
+		});
+
+	app.controller("BodyController", function() {
+		var self = this;
+		this.main = "index";
+		this.sidemenu = "sideMenuClose";
+
+		this.hideMenu = function() {
+			if ( self.sidemenu === "sideMenuOpen" ) {
+				self.sidemenu = "sideMenuClose";
+				console.log("menu closed");
+			}
+		}
+
+		this.setIndex = function() {
+			this.main = "index";
+		}
+		this.setProjects = function() {
+			this.sidemenu = "sideMenuOpen";
+			console.log("Menu Open");
+		};
+
+		this.index = function() {
+			return this.main === "index";
+		}
+		this.projects = function() {
+			this.sidemenu === "sideMenuOpen";
+		}
+
+	});
+
+
 	app.controller("EricularController", function(){
 
 		this.form = "Type here to change the right paragraph!";
@@ -25,60 +97,8 @@
 		this.toggleOption = function() {
 			this.option = true;
 		}
-
 	});
 
-	app.directive("navBar", function(){
-		return {
-			restrict:"E",
-			templateUrl:"../layouts/nav-bar.html"
-		};
-	});
-
-	app.directive("customDirective", function() {
-		return {
-			restrict:"E",
-			templateUrl:"../custom-directive.html"
-		};
-	});
-
-	app.directive("footer", function() {
-		return {
-			restrict:"E",
-			templateUrl:"../layouts/footer.html"
-		};
-	});
-
-	app.controller("BodyController", function() {
-		this.main = "index";
-
-		this.setIndex = function() {
-			this.main = "index";
-		}
-		this.setProjects = function() {
-			this.main = "projects";
-		}
-		this.index = function() {
-			return this.main === "index";
-		}
-		this.projects = function() {
-			return this.main === "projects";
-		}
-	});
-
-	app.directive("indexMain", function() {
-		return {
-			restrict:"E",
-			templateUrl:"../indexMain.html"
-		};
-	});
-
-		app.directive("projectsMain", function() {
-		return {
-			restrict:"E",
-			templateUrl:"../projectsMain.html"
-		};
-	});
 
 })();
 
