47fdf384a9b4a3b72c5acb8f28191320a7d2582f
Eric Nolte <amishe@gmail.com>
2014-06-25 19:13:47 -0400

    update test page

--- js/imageflip.js
+++ js/imageflip.js
@@ -2,20 +2,22 @@
 var flipButton = $("#flipButton");
 var effectReady = true;
 
-function flip(photo, speed)  {
+// This does a kinda hacky piggyback animation step on top of animating 'placeholder' //
+// placeholder mearly provides the 'now' and 'fx' parameters for the step function //
+// RotateX is applied over 'speed' ms for 'degs' amount //
+function flip(photo, speed, degs)  {
 
-	$(photo).animate({ textIndent: 360 }, {
+	$(photo).animate({ placeholder: degs || 360 }, {
 		start:function() {
-			flipButton.fadeTo("fast", 0.5);
+            flipButton.fadeTo("fast", 0.5);
 		},
-    step: function(now,fx) {
-    		$(this).css('opacity', now/360)
+    step: function(now, fx) {
         $(this).css('-webkit-transform',"rotateX(" + now + "deg)");
     },
-    duration:speed || 2000,
+    duration:speed || 1500,
     complete: function() {
-    	$(this).css('-webkit-transform', "rotateX(0deg)");
-    	$(this).css('textIndent', "0");
+        $(this).css('placeholder', "0");
+        $(this).css('-webkit-transform', "rotateX(0deg)");
     	flipButton.fadeTo("fast", 1);
     	effectReady = !effectReady;
     }
@@ -23,12 +25,13 @@ function flip(photo, speed)  {
 }
 
 
-
+// Click event handler assigned to flip button.  Has a effect ready check to prevent queues from stacking up. //
 flipButton.click(function() {
 	if ( effectReady === true) {
 		effectReady = !effectReady;
 		var speed = parseInt($("#speed").val());
+        var degs = parseInt($("#degs").val());
 		var photo = $("#ericGarretPic");
-		flip(photo, speed);
+		flip(photo, speed, degs);
 	}
 });
